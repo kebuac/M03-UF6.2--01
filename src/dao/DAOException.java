95193afb3ba155bc443a19c4a742555f7a51ff61
Ruben Heras <chromecrownmstr@gmail.com>
2016-04-29 00:35:36 +0200

    Commit Main
    Commit Main

--- src/dao/DAOException.java
+++ src/dao/DAOException.java
@@ -9,6 +9,9 @@ package dao;
  *
  * @author chromecrown
  */
-public class DAOException {
-    
+public class DAOException extends Exception {
+
+    public DAOException(String message) {
+        super(message);
+    }
 }
--- src/m03uf6201/M03UF6201.java
+++ src/m03uf6201/M03UF6201.java
@@ -1,16 +1,45 @@
 package m03uf6201;
 
+import dao.ClientDAO;
+import dao.ClientDAOFactory;
+import dao.ClientDAOImplem;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jdk.nashorn.internal.runtime.regexp.JoniRegExp.Factory;
+import oracle.sql.ARRAY;
+import oracle.sql.STRUCT;
+import oracle.sql.StructDescriptor;
+import singleton.ConnectionInformation;
+import singleton.DatabaseConnection;
 
 public class M03UF6201 {
 
  
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        
+        oracle.sql.STRUCT client;
+        String cif,aux;
+        Float discount;
+      
         
         try {
             
+       ClientDAOFactory daofactory = new ClientDAOFactory();
+       
+       ClientDAOImplem clientDaoImplem =  daofactory.createClientDAO();
+       
+      
+       Connection con = DatabaseConnection.getInstance();
+
+       
+       
             int opcion;
 
             
@@ -25,17 +54,28 @@ public class M03UF6201 {
                 switch (opcion) {
 
                     case 1:
-                     
+                        
+                            clientDaoImplem.showAllClients(con);
+
                         break;
 
                     case 2:
-                        
-                   
+                   client = inputClient(stdin, con);
+                   clientDaoImplem.addClient(client, con);
                         break;
                     case 3:
-                        
+                        System.out.println("Introduce cif para el cliente");
+                        cif = stdin.readLine();
+
+                        clientDaoImplem.updateClientPhone(input, con);
                         break;
                     case 4:
+                        System.out.println("Introduce cif para el cliente");
+                        cif = stdin.readLine();
+                        System.out.println("Introduce nuevo descuento para el cliente");
+                        aux = stdin.readLine();
+                        discount = Float.parseFloat(aux);
+                        clientDaoImplem.updateClientDiscount(cif, discount, con);
                         
                         break;
                     case 5:
@@ -61,13 +101,35 @@ public class M03UF6201 {
 
             System.out.println("Error" + ex);
 
+        } catch (SQLException ex) {
+            Logger.getLogger(M03UF6201.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
     
+    private static oracle.sql.STRUCT inputClient(BufferedReader stdin, Connection con) throws IOException, SQLException{
+    String cif,name,surnames,street,town,postalcode,province,aux;
+    Float discount;
+    String[] phones= new String[0];
+    String objectName = "CLIENTE_T";
+    cif = stdin.readLine();
+    name = stdin.readLine();
+    surnames = stdin.readLine();
+    street = stdin.readLine();
+    town = stdin.readLine();
+    postalcode = stdin.readLine();
+    province = stdin.readLine();
+    aux = stdin.readLine();
+    discount = Float.parseFloat(aux);
+    phones[0]=stdin.readLine();
+    StructDescriptor structDescriptor = StructDescriptor.createDescriptor(objectName, con);
+    Object[] attributes = new Object[]{cif,name,surnames,street,town,postalcode,province,discount,phones};
+    oracle.sql.STRUCT object = new oracle.sql.STRUCT(structDescriptor, con, attributes);
+    return object;
+    }
 
 }
-    
+
     
     
 
