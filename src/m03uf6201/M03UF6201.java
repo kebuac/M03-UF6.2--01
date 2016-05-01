a91c8a8bdf20cb84e2bde92defe0481fad869ab8
Ruben Heras <chromecrownmstr@gmail.com>
2016-05-01 13:37:04 +0200

    Comió casi finalización main
    Main con validados implementados y sin errores

--- src/m03uf6201/M03UF6201.java
+++ src/m03uf6201/M03UF6201.java
@@ -21,62 +21,56 @@ import singleton.DatabaseConnection;
 
 public class M03UF6201 {
 
- 
-    public static void main(String[] args){
-        
-        oracle.sql.STRUCT client;
-        String cif,aux;
-        Float discount;
-      
-        
-        try {
-            
-       ClientDAOFactory daofactory = new ClientDAOFactory();
-       
-       ClientDAOImplem clientDaoImplem =  daofactory.createClientDAO();
-       
-      
-       Connection con = DatabaseConnection.getInstance();
+    public static void main(String[] args) {
 
-       
-       
+        oracle.sql.STRUCT client;
+        String cif, aux;
+        Float discount;
+
+        try {
+
+            ClientDAOFactory daofactory = new ClientDAOFactory();
+
+            ClientDAOImplem clientDaoImplem = daofactory.createClientDAO();
+
+            Connection con = DatabaseConnection.getInstance();
+
             int opcion;
 
-            
             BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 
             do {
-               
-            System.out.println("Pulse 1 para ver clientes." + "\n" + "Pulse 2 para dar de alta clientes." + "\n" + "Pulse 3 para actualizar numero de cliente." + "\n" + "Pulse 4 para ver actualizar descuento de cliente." + "\n" + "Pulse 5 para salir del programa" + "\n");
-            String input = stdin.readLine();
-            opcion = Integer.parseInt(input);
-            
+
+                System.out.println("Pulse 1 para ver clientes." + "\n" + "Pulse 2 para dar de alta clientes." + "\n" + "Pulse 3 para actualizar numero de cliente." + "\n" + "Pulse 4 para ver actualizar descuento de cliente." + "\n" + "Pulse 5 para salir del programa" + "\n");
+                String input = stdin.readLine();
+                opcion = Integer.parseInt(input);
+
                 switch (opcion) {
 
                     case 1:
-                        
-                            clientDaoImplem.showAllClients(con);
+
+                        clientDaoImplem.showAllClients(con);
 
                         break;
 
                     case 2:
-                   client = inputClient(stdin, con);
-                   clientDaoImplem.addClient(client, con);
+                        client = inputClient(stdin, con);
+                        clientDaoImplem.addClient(client, con);
                         break;
                     case 3:
                         System.out.println("Introduce cif para el cliente");
-                        cif = stdin.readLine();
+                        cif = validarString(stdin);
 
                         clientDaoImplem.updateClientPhone(input, con);
                         break;
                     case 4:
                         System.out.println("Introduce cif para el cliente");
-                        cif = stdin.readLine();
+                        cif = validarString(stdin);
                         System.out.println("Introduce nuevo descuento para el cliente");
                         aux = stdin.readLine();
-                        discount = Float.parseFloat(aux);
+                        discount = validarFloat(stdin);
                         clientDaoImplem.updateClientDiscount(cif, discount, con);
-                        
+
                         break;
                     case 5:
 
@@ -84,9 +78,9 @@ public class M03UF6201 {
 
                          {
                             try {
-                                
+
                                 stdin.close();
-                                
+
                             } catch (IOException ex) {
                                 System.out.println("Error" + ex);
                             }
@@ -96,7 +90,7 @@ public class M03UF6201 {
                 }
 
             } while (opcion != 5);
-        
+
         } catch (IOException ex) {
 
             System.out.println("Error" + ex);
@@ -106,30 +100,160 @@ public class M03UF6201 {
         }
 
     }
-    
-    private static oracle.sql.STRUCT inputClient(BufferedReader stdin, Connection con) throws IOException, SQLException{
-    String cif,name,surnames,street,town,postalcode,province,aux;
-    Float discount;
-    String[] phones= new String[0];
-    String objectName = "CLIENTE_T";
-    cif = stdin.readLine();
-    name = stdin.readLine();
-    surnames = stdin.readLine();
-    street = stdin.readLine();
-    town = stdin.readLine();
-    postalcode = stdin.readLine();
-    province = stdin.readLine();
-    aux = stdin.readLine();
-    discount = Float.parseFloat(aux);
-    phones[0]=stdin.readLine();
-    StructDescriptor structDescriptor = StructDescriptor.createDescriptor(objectName, con);
-    Object[] attributes = new Object[]{cif,name,surnames,street,town,postalcode,province,discount,phones};
-    oracle.sql.STRUCT object = new oracle.sql.STRUCT(structDescriptor, con, attributes);
-    return object;
+
+    private static oracle.sql.STRUCT inputClient(BufferedReader stdin, Connection con) throws IOException, SQLException {
+        String cif, name, surnames, street, town, postalcode, province;
+        Float discount;
+        String[] phones = new String[0];
+        String objectName = "CLIENTE_T";
+
+        System.out.println("Introduce el cif del cliente");
+
+        cif = validarString(stdin);
+        System.out.println("Introduce el nombre del cliente");
+
+        name = validarString(stdin);
+        System.out.println("Introduce el name del cliente");
+
+        surnames = validarString(stdin);
+        System.out.println("Introduce el surnames del cliente");
+
+        street = validarString(stdin);
+        System.out.println("Introduce el street del cliente");
+
+        town = validarString(stdin);
+        System.out.println("Introduce el town del cliente");
+
+        postalcode = validarString(stdin);
+        System.out.println("Introduce el postalcode del cliente");
+
+        province = validarString(stdin);
+        //
+        System.out.println("Introduce el descuento del cliente");
+        discount = validarFloat(stdin);
+
+        System.out.println("¿Cuántos números quieres introducir? 1, 2 o 3");
+        String input = stdin.readLine();
+
+        int auxNums = Integer.parseInt(input);
+
+        if (auxNums == 1 || auxNums == 2 || auxNums == 3) {
+
+            for (int i = 0; i < auxNums; i++) {
+                System.out.println("Introduce número del cliente");
+
+                String aux = "" + validarInt(stdin);
+
+                phones[i] = aux;
+            }
+
+        } else {
+
+            phones[0] = null;
+
+        }
+        
+        StructDescriptor structDescriptor = StructDescriptor.createDescriptor(objectName, con);
+        Object[] attributes = new Object[]{cif, name, surnames, street, town, postalcode, province, discount, phones};
+        oracle.sql.STRUCT object = new oracle.sql.STRUCT(structDescriptor, con, attributes);
+        return object;
+    }
+
+    public static String validarString(BufferedReader stdin) {
+
+        boolean validacion = false;
+        String input = null;
+        do {
+
+            try {
+
+                input = stdin.readLine();
+
+                if (input.matches("[a-z]*")) {
+
+                    validacion = true;
+                } else {
+
+                    System.out.println("Argumento no valido, introduza el dato nuevamente");
+                }
+
+            } catch (IOException ex) {
+
+                System.out.println("Error al leer los datos");
+            }
+
+        } while (validacion = false);
+
+        return input;
+
+    }
+
+    public static float validarFloat(BufferedReader stdin) {
+
+        boolean validacion = false;
+        String input = null;
+        float flo = 0;
+        float flocompro = (float) -0.000001;
+        do {
+
+            try {
+
+                input = stdin.readLine();
+
+                if (input.matches("[0-9]*")) {
+
+                    flo = Float.parseFloat(input);
+                    validacion = true;
+                } else if (input.matches("[0-9]+.[0-9]*")) {
+
+                    flo = Float.parseFloat(input);
+                    validacion = true;
+                } else {
+
+                    System.out.println("Argumento no valido, introduza el dato nuevamente");
+                }
+
+            } catch (IOException ex) {
+
+                System.out.println("Error al leer los datos");
+            }
+
+        } while (validacion = false && flo > flocompro);
+
+        return flo;
+
+    }
+
+    public static int validarInt(BufferedReader stdin) {
+
+        boolean validacion = false;
+        String input = null;
+        int inte = 0;
+        int flocompro = (int) -0.000001;
+        do {
+
+            try {
+
+                input = stdin.readLine();
+
+                if (input.matches("[0-9]*")) {
+
+                    inte = Integer.parseInt(input);
+                    validacion = true;
+                } else {
+
+                    System.out.println("Argumento no valido, introduza el dato nuevamente");
+                }
+
+            } catch (IOException ex) {
+
+                System.out.println(ex + " Error al leer los datos");
+            }
+
+        } while (validacion = false && inte > flocompro);
+
+        return inte;
+
     }
 
 }
-
-    
-    
-
